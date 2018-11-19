5353e58efc352924283dd297ee0cb773ed4f57d7
AlexandrAlexeev <a.joy8508@gmail.com>
2018-11-19 20:54:07 +0300

    1.3. JavaDoc. Code convention[#87105]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ * @author Alexandr Alexeev (a.joy8508@gmail.com)
+ */
 public class Calculate{
-
+   /**
+    * Main.
+	*@param args - args.
+	*/
    public static void main(String[] args){
 	System.out.println("Hello World");
    }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexandr Alexeev (a.joy8508@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
