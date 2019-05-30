548eccd287ab11f6c848d8b551a9479228d6e000
AlexandrAlexeev <a.joy8508@gmail.com>
2019-05-30 16:13:21 +0300

    5.3. Построить шахматную доску в псевдографике.[#87080]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+/**
+ * Class generates checkmate board
+ * @author Alexandr Alexeev 
+ * @version %Id%
+ * @since 0.1
+ */
+public class Board {
+    /**
+     * Assembly a string representation of a checkmate board
+     * @param width Board width
+     * @param height Board height
+     * @return Checkmate board
+     */
+    public String paint(int width, int height) {
+        StringBuilder result = new StringBuilder();
+        String separator = System.getProperty("line.separator");
+        for (int i = 0; i < height; i++) {
+            for (int k = 0; k < width; k++) {
+                if ((i + k) % 2 == 0) {
+                    result.append('x');
+                } else {
+                    result.append(' ');
+                }
+            }
+            result.append(separator);
+        }
+        return result.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author Alexandr Alexeev 
+ * @version %Id%
+ * @since 0.1
+ */
+public class BoardTest {
+    /**
+     * Test 3x3 board
+     */
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String separator = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", separator, separator, separator);
+        assertThat(result, is(expected));
+    }
+    /**
+     * Test 5x4 board
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String separator = System.getProperty("line.separator");
+        String expected = String.format("x x x%s x x %sx x x%s x x %s", separator, separator, separator, separator);
+        assertThat(result, is(expected));
+    }
+}
