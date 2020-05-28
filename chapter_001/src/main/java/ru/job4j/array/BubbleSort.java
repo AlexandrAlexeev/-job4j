18ced8be12a52901259f200b18201889814da24b
AlexandrAlexeev <a.joy8508@gmail.com>
2020-05-28 23:52:25 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[195#87088]

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -8,39 +8,17 @@ package ru.job4j.array;
  * @since 0.1
  */
 public class BubbleSort {
+    int[] array;
 
-    private int[] array;
-    private int currentLenght;
-
-    /**
-     * BubbleSort(int[] array) - конструкторв принимает массив целочисленных примитивов.
-     * @param array - массив приметивов.
-     * @since 0.1.
-     */
-    public BubbleSort(int[] array) {
-        this.array = array;
-        this.currentLenght = array.length - 1;
-    }
-
-    /**
-     * bubbleSortIntArray() - медод сортировки массива методом перестановки (пузырька), переданного конструктору при создании массива.
-     * @return - отсортированный массив целочисленных примитивов.
-     * @since 0.1.
-     */
-    public int[] bubbleSortIntArray() {
-        int tmp;
-        while (currentLenght > 0) {
-            for (int i = 0; i < currentLenght; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (array[i] > array[i + 1]) {
-                        tmp = array[i];
-                        array[i] = array[i + 1];
-                        array[i + 1] = tmp;
-                    }
-
+    public int[] sort(int[] array) {
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - j - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
                 }
             }
-            currentLenght--;
         }
         return array;
     }
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -1,36 +1,16 @@
 package ru.job4j.array;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class BubbleSortTest {
     @Test
     public void whenSortArrayWithTenElementsThenSortedArray() {
-        //проверка сортировки массива из 10 элементов методом пузырька, например {1, 5, 4, 2, 3, 1, 7, 8, 0, 5}.
-        int[] inputArray = {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
-        BubbleSort bSort = new BubbleSort(inputArray);
-        inputArray = bSort.bubbleSortIntArray();
-        int[] expectArray = {0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
-        assertThat(expectArray, is(inputArray));
-    }
-
-    @Test
-    public void whenSortArrayWithTenElementsThenSortedArray2() {
-        int[] inputArray = {5, 1, 2, 7, 3};
-        BubbleSort bSort = new BubbleSort(inputArray);
-        int[] expectArray = {1, 2, 3, 5, 7};
-        inputArray = bSort.bubbleSortIntArray();
-        assertThat(expectArray, is(inputArray));
-    }
-
-    @Test
-    public void whenSortArrayWithTenElementsThenSortedArray3() {
-        int[] inputArray = {5, 1, 2, 7, 3};
-        BubbleSort bSort = new BubbleSort(inputArray);
-        int[] expectArray = {1, 2, 3, 5, 7};
-        inputArray = bSort.bubbleSortIntArray();
-        assertThat(inputArray, is(expectArray));
+        BubbleSort sorter = new BubbleSort();
+        int[] array = new int[]{5, 10, 3, 2, 4, 9, 8, 7, 6, 1};
+        int[] result = sorter.sort(array);
+        int[] expect = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(result, Matchers.is(expect));
     }
 }
